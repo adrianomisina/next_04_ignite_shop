5754ab775433dd87af569854db255633122b14c4
Adriano Misina <adriano.misina@agroboard.com.br>
2023-09-12 09:44:12 -0300

    Cabeçalho da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
-import { AppProps } from "next/app"
-import { globalStyles } from "../styles/global"
+import { AppProps } from "next/app";
+import Image from "next/image";
+import { globalStyles } from "../styles/global";
+import logoImg from '../assets/Logo.svg'
+import { Container, Header } from "../styles/pages/app";
 
 globalStyles()
 
 export default function App({ Component, pageProps}:AppProps ) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="logo Ignite Shop" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
