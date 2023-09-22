dc5abf3e906b8f0d47084f4036f722a6bdfe7c1c
Adriano Misina <adriano.misina@agroboard.com.br>
2023-09-22 09:48:29 -0300

    estrutura pag produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,29 @@
 import { useRouter } from "next/router";
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product";
 
 const Product = () => {
     const {query} = useRouter()
     return (
-        <h1>Product: {JSON.stringify(query)}</h1>
+        <ProductContainer>
+            <ImageContainer>
+
+            </ImageContainer>
+
+            <ProductDetails>
+                <h1>Camiseta X</h1>
+                <span>R$ 79,90</span>
+
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Repellendus assumenda explicabo porro est amet illo inventore soluta unde quam modi quod illum aspernatur,
+                    laudantium blanditiis provident iusto, qui doloribus quis!
+                </p>
+
+                <button>
+                    Comprar agora
+                </button>
+            </ProductDetails>
+        </ProductContainer>
     )
 }
 
